317d5438a1ca72378b6278d00fe4fc3b19706ee4
Cleber Neitzke <clneitzke@gmail.com>
2025-08-02 14:51:28 -0700

    Add Projects API controller and update namespaces
    Implemented ProjectsController with GET and POST endpoints for managing projects in the API. Updated ProjectService and SkillService namespaces to SkillSnap.Client.Services. Added sample GET request for projects to SkillSnap.Api.http. Minor formatting update to ProfileCard.razor.

--- SkillSnap.Api/Controllers/ProjectsController.cs
+++ SkillSnap.Api/Controllers/ProjectsController.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using SkillSnap.Api.Data;
+using SkillSnap.Shared.Models;
+
 namespace SkillSnap.Api.Controllers;
 
-public class ProjectsController
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController : ControllerBase
 {
-    
+    private readonly SkillSnapContext _context;
+
+    public ProjectsController(SkillSnapContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Projects
+    [HttpGet]
+    public ActionResult<IEnumerable<Project>> GetProjects()
+    {
+        return Ok(_context.Projects.ToList());
+    }
+
+    // POST: api/Projects
+    [HttpPost]
+    public ActionResult<Project> AddProject(Project project)
+    {
+        _context.Projects.Add(project);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetProjects), new { id = project.Id }, project);
+    }
 }
--- SkillSnap.Client/Services/SkillService.cs
+++ SkillSnap.Client/Services/SkillService.cs
@@ -1,4 +1,4 @@
-namespace SkillSnap.Client;
+namespace SkillSnap.Client.Services;
 
 public class SkillService
 {
